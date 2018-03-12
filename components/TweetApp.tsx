a5131b26a1a25db867633a6e1223942018a7b8eb
baldore <acornssn@gmail.com>
2018-03-12 09:19:52 -0500

    Add reducer for TweetApp

--- components/TweetApp.tsx
+++ components/TweetApp.tsx
@@ -21,29 +21,76 @@ interface TweetsAppProps {
   count: number
 }
 
+type UpdateTweetsAction = {
+  type: 'update-tweets'
+  tweets: Tweet[]
+}
+
+type UpdateCountAction = {
+  type: 'update-count'
+  count: number
+}
+
+type TweetsAppActions = UpdateTweetsAction | UpdateCountAction
+
+function tweetsAppReducer(
+  state: TweetsAppProps,
+  action: TweetsAppActions,
+): TweetsAppProps {
+  switch (action.type) {
+    case 'update-tweets':
+      return {
+        ...state,
+        tweets: action.tweets,
+      }
+
+    case 'update-count':
+      return {
+        ...state,
+        count: action.count,
+      }
+
+    default:
+      return state
+  }
+}
+
+const initialState: TweetsAppProps = {
+  count: 0,
+  tweets: [],
+}
+
 const TweetsAppStream = componentFromStream(
   (propsStream: Subscribable<TweetsAppProps>) => {
     const socket = io('http://localhost:3000')
     const newTweets$ = Observable.fromEvent<Tweet>(socket, socketEvents.tweet)
-    // To see that they actually appear on screen
-    // .map(tweet => ({ ...tweet, active: true }))
-
     const props$ = Observable.from(propsStream)
-    const initialTweets$ = props$.map(props => props.tweets)
 
-    const combinedTweets$ = initialTweets$.switchMap(initialTweets =>
+    const initialTweets$ = props$.map(props => props.tweets)
+    const allTweets$ = initialTweets$.switchMap(initialTweets =>
       newTweets$
         .scan((tweets, newTweet) => [newTweet, ...tweets], initialTweets)
         .startWith(initialTweets),
     )
 
-    const finalProps$ = Observable.combineLatest<TweetsAppProps>(
-      combinedTweets$,
-      (tweets: Tweet[]) => ({
+    const updateTweetsAction$ = allTweets$.map<Tweet[], UpdateTweetsAction>(
+      tweets => ({
+        type: 'update-tweets',
         tweets,
+      }),
+    )
+
+    const updateCountAction$ = allTweets$.map<Tweet[], UpdateCountAction>(
+      tweets => ({
+        type: 'update-count',
         count: tweets.filter(tweet => tweet.active === false).length,
       }),
     )
+
+    const finalProps$ = Observable.merge(
+      updateTweetsAction$,
+      updateCountAction$,
+    ).scan(tweetsAppReducer, initialState)
 
     return finalProps$.map(props => <TweetsApp {...props} />)
   },
